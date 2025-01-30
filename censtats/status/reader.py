39ea05fd7b19f5767d2c7124ea432858d5bbb304
Keisuke Oshima <koisland@sarlacc.pmacs.upenn.edu>
2025-01-30 02:20:52 -0500

    Fix columns change.

--- censtats/status/reader.py
+++ censtats/status/reader.py
@@ -4,15 +4,11 @@ from .constants import RM_COLS, RM_COL_IDX
 
 
 def read_repeatmasker_output(input_path: TextIO | str) -> pl.DataFrame:
-    return (
-        pl.read_csv(
-            input_path,
-            separator="\t",
-            has_header=False,
-            columns=RM_COL_IDX,
-            new_columns=RM_COLS,
-            truncate_ragged_lines=True,
-        )
-        .with_columns(dst=pl.col("end") - pl.col("start"))
-        .drop("div", "deldiv", "insdiv", "x", "y", "z", "left", "right", "idx")
-    )
+    return pl.read_csv(
+        input_path,
+        separator="\t",
+        has_header=False,
+        columns=RM_COL_IDX,
+        new_columns=RM_COLS,
+        truncate_ragged_lines=True,
+    ).with_columns(dst=pl.col("end") - pl.col("start"))
